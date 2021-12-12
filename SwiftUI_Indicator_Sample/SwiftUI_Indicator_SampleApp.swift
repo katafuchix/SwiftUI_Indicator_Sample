6663add8bdbffe89bf2db62ea6b05f413e7e9f60
katafuchix <katafuchix@gmail.com>
2021-12-12 10:37:13 +0900

    Initial Commit

---
+++ SwiftUI_Indicator_Sample/SwiftUI_Indicator_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Indicator_SampleApp.swift
+//  SwiftUI_Indicator_Sample
+//
+//  Created by cano on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Indicator_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
